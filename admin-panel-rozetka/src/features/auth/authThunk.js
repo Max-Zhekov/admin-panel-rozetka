cdfd1f6eafc62c2a9b49702cfaa09608d7f5dfa8
Max-Jekov <maxzhekov07@gmail.com>
2025-08-07 21:19:20 +0300

    jwt login

--- admin-panel-rozetka/src/features/auth/authThunk.js
+++ admin-panel-rozetka/src/features/auth/authThunk.js
@@ -5,13 +5,11 @@ export const loginThunk = createAsyncThunk(
   "auth/login",
   async ({ name, password }, { rejectWithValue }) => {
     try {
-      const res = await axios.get(
-        "https://689235a7447ff4f11fbf900b.mockapi.io/Admins"
-      );
-      const user = res.data.find(
-        (u) => u.name === name && u.password === password
-      );
-      if (user) return user;
+      const res = await axios.post("http://fak.pp.ua:81/api/auth/login", {
+        email: name,
+        password: password,
+      });
+      if (res) return res;
       return rejectWithValue("Неверное имя или пароль");
     } catch (error) {
       return rejectWithValue("Ошибка при авторизации");
